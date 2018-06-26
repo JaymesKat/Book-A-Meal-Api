e2e6524806d1cbfde2106ef2ef6bf43ab9fc6a71
James Katarikawe <>
2018-06-26 10:33:20 +0300

    Removed redundant code in app/entities/meals.py

--- app/entities/meals.py
+++ app/entities/meals.py
@@ -6,27 +6,6 @@ class Meal(object):
         self.name = name
         self.price = price
 
-
-    '''Add a new meal'''
-    def save(self, id, name, price):
-        pass
-
-    '''Get a meal by id'''
-    def get_by_id(self, id):
-        pass
-        
-    '''Get list of all meals'''
-    def get(self):
-        pass
-
-    '''Update a meal'''
-    def update(self, id, name,price):
-        pass
-
-    '''Delete a meal'''
-    def delete(self):
-        pass
-
     def serialize(self):
         return {
             'id': self.id, 
